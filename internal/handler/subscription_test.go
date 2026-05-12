58167ce2b9f39a789e5221b26642b873700a078d
JMurad <jml.murad@gmail.com>
2026-05-12 10:50:28 +0300

    use service interface in subscription handler tests and add create subscription success test

--- internal/handler/subscription_test.go
+++ internal/handler/subscription_test.go
@@ -7,18 +7,17 @@ import (
 	"testing"
 
 	"subscriptions-service/internal/handler"
-	"subscriptions-service/internal/service"
 	"subscriptions-service/internal/service/mocks"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
 func TestCreateSubscription_InvalidJSON(t *testing.T) {
 	mockRepo := new(mocks.Repository)
 
-	svc := service.NewSubscriptionService(mockRepo)
-
-	h := handler.NewSubscriptionHandler(svc)
+	h := handler.NewSubscriptionHandler(mockRepo)
 
 	body := `invalid-json`
 
@@ -42,13 +41,11 @@ func TestCreateSubscription_InvalidJSON(t *testing.T) {
 func TestCreateSubscription_InvalidUUID(t *testing.T) {
 	mockRepo := new(mocks.Repository)
 
-	svc := service.NewSubscriptionService(mockRepo)
-
-	h := handler.NewSubscriptionHandler(svc)
+	h := handler.NewSubscriptionHandler(mockRepo)
 
 	body := `{
 		"service_name":"Yandex Plus",
-		"price":500,
+		"price":400,
 		"user_id":"invalid",
 		"start_date":"07-2025"
 	}`
@@ -73,13 +70,11 @@ func TestCreateSubscription_InvalidUUID(t *testing.T) {
 func TestCreateSubscription_InvalidDate(t *testing.T) {
 	mockRepo := new(mocks.Repository)
 
-	svc := service.NewSubscriptionService(mockRepo)
-
-	h := handler.NewSubscriptionHandler(svc)
+	h := handler.NewSubscriptionHandler(mockRepo)
 
 	body := `{
 		"service_name":"Yandex Plus",
-		"price":500,
+		"price":400,
 		"user_id":"60601fee-2bf1-4721-ae6f-7636e79a0cba",
 		"start_date":"invalid-date"
 	}`
@@ -99,4 +94,37 @@ func TestCreateSubscription_InvalidDate(t *testing.T) {
 		http.StatusBadRequest,
 		w.Code,
 	)
+}
+
+func TestCreateSubscription_Success(t *testing.T) {
+	mockRepo := new(mocks.Repository)
+
+	h := handler.NewSubscriptionHandler(mockRepo)
+
+	expectedID := uuid.New()
+
+	mockRepo.
+		On("Create", mock.Anything, mock.Anything).
+		Return(expectedID, nil)
+
+	body := `{
+		"service_name":"Yandex Plus",
+		"price":400,
+		"user_id":"60601fee-2bf1-4721-ae6f-7636e79a0cba",
+		"start_date":"07-2025"
+	}`
+
+	req := httptest.NewRequest(
+		http.MethodPost,
+		"/subscriptions",
+		bytes.NewBufferString(body),
+	)
+
+	w := httptest.NewRecorder()
+
+	h.Create(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	mockRepo.AssertExpectations(t)
 }
