6f06ad994a1df148a6be1541c5b6946db0e2a4e1
JMurad <jml.murad@gmail.com>
2026-05-12 12:28:52 +0300

    add subscription list service

--- internal/service/subscriptions/service.go
+++ internal/service/subscriptions/service.go
@@ -26,3 +26,7 @@ func (s *SubService) Create(ctx context.Context, sub model.Subscription) (uuid.U
 func (s *SubService) GetByID(ctx context.Context, id uuid.UUID) (*model.Subscription, error) {
 	return s.repo.GetByID(ctx, id)
 }
+
+func (s *SubService) List(ctx context.Context, limit, offset int) ([]model.Subscription, error) {
+	return s.repo.List(ctx, limit, offset)
+}
