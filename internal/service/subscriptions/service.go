e82f02e54ae0c8a0951c8c22525fa85951882d6a
JMurad <jml.murad@gmail.com>
2026-05-12 09:49:10 +0300

    fix implement subscription service layer

---
+++ internal/service/subscriptions/service.go
@@ -0,0 +1,28 @@
+package service
+
+import (
+	"context"
+
+	"subscriptions-service/internal/model"
+	"subscriptions-service/internal/repository"
+
+	"github.com/google/uuid"
+)
+
+type SubService struct {
+	repo repository.SubscriptionRepository
+}
+
+func NewSubscriptionService(repo repository.SubscriptionRepository) *SubService {
+	return &SubService{
+		repo: repo,
+	}
+}
+
+func (s *SubService) Create(ctx context.Context, sub model.Subscription) (uuid.UUID, error) {
+	return s.repo.Create(ctx, sub)
+}
+
+func (s *SubService) GetByID(ctx context.Context, id uuid.UUID) (*model.Subscription, error) {
+	return s.repo.GetByID(ctx, id)
+}
