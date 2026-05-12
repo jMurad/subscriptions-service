4e3d7e14197e038bfb203321906b5f5fad514601
JMurad <jml.murad@gmail.com>
2026-05-12 12:29:59 +0300

    add subscription list repo pg

--- internal/repository/postgres/subscription.go
+++ internal/repository/postgres/subscription.go
@@ -73,3 +73,54 @@ func (r *SubRepo) GetByID(ctx context.Context, id uuid.UUID) (*model.Subscriptio
 
 	return &sub, nil
 }
+
+func (r *SubRepo) List(ctx context.Context, limit, offset int) ([]model.Subscription, error) {
+	query := `
+	SELECT
+		id,
+		service_name,
+		price,
+		user_id,
+		start_date,
+		end_date,
+		created_at
+	FROM subscriptions
+	ORDER BY created_at DESC
+	LIMIT $1 OFFSET $2
+	`
+
+	rows, err := r.db.Query(
+		ctx,
+		query,
+		limit,
+		offset,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var subscriptions []model.Subscription
+
+	for rows.Next() {
+
+		var sub model.Subscription
+
+		err = rows.Scan(
+			&sub.ID,
+			&sub.ServiceName,
+			&sub.Price,
+			&sub.UserID,
+			&sub.StartDate,
+			&sub.EndDate,
+			&sub.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		subscriptions = append(subscriptions, sub)
+	}
+
+	return subscriptions, nil
+}
