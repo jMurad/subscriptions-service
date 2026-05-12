e3d81d79bae68f4f4c22c734a19cdeae0a109ad1
JMurad <jml.murad@gmail.com>
2026-05-12 11:35:59 +0300

    add gracefully shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,36 +2,39 @@ package main
 
 import (
 	"context"
-	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"subscriptions-service/internal/config"
 	"subscriptions-service/internal/handler"
-	"subscriptions-service/internal/logger"
 	"subscriptions-service/internal/repository/postgres"
 	"subscriptions-service/internal/service/subscriptions"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"go.uber.org/zap"
 )
 
 func main() {
+	// --- load environment ---
 	cfg, err := config.Load()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	logg, err := logger.New()
+	// --- log ---
+	logg, err := zap.NewProduction()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-
-	defer logg.Sync()
 
 	// --- DB ---
 	db, err := pgxpool.New(context.Background(), cfg.DBUrl)
 	if err != nil {
-		log.Fatal(err)
+		logg.Fatal("failed to connect db", zap.Error(err))
 	}
 
 	defer db.Close()
@@ -48,12 +51,51 @@ func main() {
 	// --- router ---
 	r := chi.NewRouter()
 
-	r.Post("/subscriptions", h.Create)
+	r.Route("/subscriptions", func(r chi.Router) {
+		r.Post("/", h.Create)
+		r.Get("/{id}", h.GetByID)
+	})
 
-	logg.Info("server started")
-
-	err = http.ListenAndServe(":"+cfg.AppPort, r)
-	if err != nil {
-		log.Fatal(err)
+	// Configure HTTP server
+	srv := &http.Server{
+		Addr:    ":" + cfg.AppPort,
+		Handler: r,
 	}
+
+	// Start HTTP server in background goroutine
+	go func() {
+		logg.Info("server started", zap.String("port", cfg.AppPort))
+
+		if err := srv.ListenAndServe(); err != nil &&
+			err != http.ErrServerClosed {
+			logg.Fatal("server failed", zap.Error(err))
+		}
+	}()
+
+	// Wait for termination signal
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(
+		quit,
+		syscall.SIGINT,
+		syscall.SIGTERM,
+	)
+
+	<-quit
+
+	// Gracefully shutdown server
+	logg.Info("shutdown server")
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		logg.Fatal("shutdown failed", zap.Error(err))
+	}
+
+	logg.Info("server stopped")
 }
