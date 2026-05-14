ac70e8861d6f6dad24c3b92f3b7060732464e6ab
JMurad <jml.murad@gmail.com>
2026-05-14 16:07:26 +0300

    move startup logic to internal app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,110 +1,13 @@
 package main
 
 import (
-	"context"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+	"log"
 
-	"subscriptions-service/internal/config"
-	"subscriptions-service/internal/handler"
-	"subscriptions-service/internal/middleware"
-	"subscriptions-service/internal/repository/postgres"
-	"subscriptions-service/internal/service/subscriptions"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/jackc/pgx/v5/pgxpool"
-	"go.uber.org/zap"
+	"subscriptions-service/internal/app"
 )
 
 func main() {
-	// --- load environment ---
-	cfg, err := config.Load()
-	if err != nil {
-		panic(err)
+	if err := app.Run(); err != nil {
+		log.Fatal(err)
 	}
-
-	// --- log ---
-	logg, err := zap.NewProduction()
-	if err != nil {
-		panic(err)
-	}
-
-	// --- DB ---
-	db, err := pgxpool.New(context.Background(), cfg.DBUrl)
-	if err != nil {
-		logg.Fatal("failed to connect db", zap.Error(err))
-	}
-
-	defer db.Close()
-
-	// --- repositories ---
-	repo := postgres.NewSubscriptionRepository(db)
-
-	// --- services ---
-	svc := subscriptions.NewSubscriptionService(repo)
-
-	// --- handlers ---
-	h := handler.NewSubscriptionHandler(svc)
-
-	// --- router ---
-	r := chi.NewRouter()
-
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger(logg))
-	r.Use(middleware.Recovery(logg))
-
-	r.Route("/subscriptions", func(r chi.Router) {
-		r.Post("/", h.Create)
-		r.Get("/{id}", h.GetByID)
-		r.Get("/", h.List)
-		r.Get("/total", h.Total)
-		r.Patch("/{id}", h.Update)
-		r.Delete("/{id}", h.Delete)
-	})
-
-	// Configure HTTP server
-	srv := &http.Server{
-		Addr:    ":" + cfg.AppPort,
-		Handler: r,
-	}
-
-	// Start HTTP server in background goroutine
-	go func() {
-		logg.Info("server started", zap.String("port", cfg.AppPort))
-
-		if err := srv.ListenAndServe(); err != nil &&
-			err != http.ErrServerClosed {
-			logg.Fatal("server failed", zap.Error(err))
-		}
-	}()
-
-	// Wait for termination signal
-	quit := make(chan os.Signal, 1)
-
-	signal.Notify(
-		quit,
-		syscall.SIGINT,
-		syscall.SIGTERM,
-	)
-
-	<-quit
-
-	// Gracefully shutdown server
-	logg.Info("shutdown server")
-
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		5*time.Second,
-	)
-
-	defer cancel()
-
-	if err := srv.Shutdown(ctx); err != nil {
-		logg.Fatal("shutdown failed", zap.Error(err))
-	}
-
-	logg.Info("server stopped")
 }
