6be748e89f8cc1d3ec65a6d46d3867f3b7509927
JMurad <jml.murad@gmail.com>
2026-05-16 19:41:51 +0300

    refactor Update service add logic and error handling

--- internal/service/subscriptions/service.go
+++ internal/service/subscriptions/service.go
@@ -176,24 +176,79 @@ offset,
 }
 
 func (s *SubService) Update(ctx context.Context, id uuid.UUID, update model.SubscriptionUpdate) error {
+ctx, cancel := context.WithTimeout(ctx, shortTimeout)
+	defer cancel()
+
 	log := logger.FromContext(ctx)
-	start := time.Now()
+	
+	if update.ServiceName == nil && update.Price == nil && update.EndDate == nil {
+		err := apperrors.WrapMessage(
+			nil,
+			apperrors.ErrValidation,
+			"at least one field is required",
+		)
 
-	log.Info("service update subscription started", zap.String("subscription_id", id.String()))
-
-	err := s.repo.Update(ctx, id, update)
-	if err != nil {
-		log.Error("service update subscription failed",
-			zap.String("subscription_id", id.String()),
+	log.Warn("validation failed",
 			zap.Error(err),
 		)
+
 		return err
 	}
 
-	log.Info("service update subscription completed",
-		zap.String("subscription_id", id.String()),
-		zap.Duration("duration", time.Since(start)),
+	if update.ServiceName != nil {
+		trimmed := strings.TrimSpace(*update.ServiceName)
+
+		if trimmed == "" {
+			err := apperrors.WrapMessage(
+				nil,
+				apperrors.ErrValidation,
+				"service_name cannot be empty",
+			)
+
+			log.Warn("validation failed",
+				zap.String("field", "service_name"),
+				zap.Error(err),
+			)
+
+			return err
+		}
+
+		update.ServiceName = &trimmed
+	}
+
+	if update.Price != nil && *update.Price <= 0 {
+		err := apperrors.WrapMessage(
+			nil,
+			apperrors.ErrValidation,
+			"price must be greater than 0",
+		)
+
+		log.Warn("validation failed",
+			zap.String("field", "price"),
+			zap.Error(err),
+		)
+
+		return err
+	}
+
+	err := s.repo.Update(
+		ctx,
+		id,
+		update,
 	)
+	if err != nil {
+		log.Error("failed to update subscription",
+			zap.String("subscription_id", id.String()),
+			zap.Error(err),
+		)
+
+		return err
+	}
+
+	log.Info(
+		"subscription updated",
+		zap.String("subscription_id", id.String()),
+			)
 
 	return nil
 }
