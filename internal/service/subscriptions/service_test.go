595f1e2a0faf98a502580a4d227a33932b053eca
JMurad <jml.murad@gmail.com>
2026-05-16 19:56:50 +0300

    refactor and add new GetByID unit tests

--- internal/service/subscriptions/service_test.go
+++ internal/service/subscriptions/service_test.go
@@ -2,18 +2,18 @@ package subscriptions_test
 
 import (
 	"context"
-		"testing"
+	"testing"
 	"time"
 
-apperrors "subscriptions-service/internal/errors"
+	apperrors "subscriptions-service/internal/errors"
 	"subscriptions-service/internal/model"
 	"subscriptions-service/internal/service/mocks"
 	"subscriptions-service/internal/service/subscriptions"
 
 	"github.com/google/uuid"
-"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/assert"
-"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/mock"
 )
 
 // Create tests
@@ -25,15 +25,15 @@ func TestCreate_Success(t *testing.T) {
 	subID := uuid.New()
 
 	sub := model.Subscription{
-				UserID:      uuid.New(),
-ServiceName: "Netflix",
+		UserID:      uuid.New(),
+		ServiceName: "Netflix",
 		Price:       999,
 		StartDate:   time.Now(),
 	}
 
 	repo.On("Create",
 		mock.Anything,
-sub,
+		sub,
 	).Return(subID, nil)
 
 	id, err := svc.Create(
@@ -82,13 +82,13 @@ func TestCreate_EmptyServiceName(t *testing.T) {
 }
 
 func TestCreate_InvalidPrice(t *testing.T) {
-repo := new(mocks.Repository)
+	repo := new(mocks.Repository)
 
 	svc := subscriptions.NewSubscriptionService(repo)
 
 	sub := model.Subscription{
-				UserID:      uuid.New(),
-ServiceName: "Netflix",
+		UserID:      uuid.New(),
+		ServiceName: "Netflix",
 		Price:       0,
 		StartDate:   time.Now(),
 	}
@@ -217,7 +217,7 @@ func TestCreate_RepositoryError(t *testing.T) {
 
 	assert.Equal(t, expectedErr, err)
 
-repo.AssertExpectations(t)
+	repo.AssertExpectations(t)
 }
 
 func TestCreate_ContextCanceled(t *testing.T) {
@@ -319,280 +319,195 @@ func TestCreate_ContextDeadlineExceeded(t *testing.T) {
 
 // GetByID tests
 func TestGetByID_Success(t *testing.T) {
-	mockRepo := new(mocks.Repository)
+	repo := new(mocks.Repository)
 
-	svc := subscriptions.NewSubscriptionService(mockRepo)
+	svc := subscriptions.NewSubscriptionService(repo)
+
+	id := uuid.New()
 
 	expected := &model.Subscription{
-		ID:          uuid.New(),
-		ServiceName: "Netflix",
-		Price:       500,
+		ID:          id,
 		UserID:      uuid.New(),
+		ServiceName: "Netflix",
+		Price:       999,
 		StartDate:   time.Now(),
+		CreatedAt:   time.Now(),
 	}
 
-	mockRepo.
-		On(
-			"GetByID",
-			context.Background(),
-			expected.ID,
-		).
-		Return(expected, nil)
+	repo.On("GetByID",
+		mock.Anything,
+		id,
+	).Return(expected, nil)
 
-	result, err := svc.GetByID(
+	sub, err := svc.GetByID(
 		context.Background(),
-		expected.ID,
+		id,
 	)
 
 	assert.NoError(t, err)
-	assert.Equal(t, expected, result)
 
-	mockRepo.AssertExpectations(t)
+	assert.Equal(t, expected, sub)
+
+	repo.AssertExpectations(t)
 }
 
 func TestGetByID_NotFound(t *testing.T) {
-	mockRepo := new(mocks.Repository)
+	repo := new(mocks.Repository)
 
-	svc := subscriptions.NewSubscriptionService(mockRepo)
+	svc := subscriptions.NewSubscriptionService(repo)
 
 	id := uuid.New()
 
-	expectedErr := errors.New("not found")
+	expectedErr := apperrors.Wrap(
+		pgx.ErrNoRows,
+		apperrors.ErrNotFound,
+	)
 
-	mockRepo.
-		On(
-			"GetByID",
-			context.Background(),
-			id,
-		).
-		Return(nil, expectedErr)
+	repo.On("GetByID",
+		mock.Anything,
+		id,
+	).Return(nil, expectedErr)
 
-	result, err := svc.GetByID(
+	sub, err := svc.GetByID(
 		context.Background(),
 		id,
 	)
 
 	assert.Error(t, err)
-	assert.Nil(t, result)
 
-	mockRepo.AssertExpectations(t)
-}
+	assert.Nil(t, sub)
 
-// List tests
-func TestList_Success(t *testing.T) {
-	mockRepo := new(mocks.Repository)
+	appErr, ok := err.(*apperrors.Error)
 
-	svc := subscriptions.NewSubscriptionService(mockRepo)
+	assert.True(t, ok)
 
-	expected := []model.Subscription{
-		{
-			ID:          uuid.New(),
-			ServiceName: "Netflix",
-			Price:       500,
-		},
-	}
-
-	mockRepo.
-		On("List", context.Background(), 10, 0).
-		Return(expected, nil)
-
-	result, err := svc.List(
-		context.Background(),
-		10,
-		0,
+	assert.Equal(
+		t,
+		apperrors.ErrNotFound.Code,
+		appErr.Code,
 	)
 
-	assert.NoError(t, err)
-	assert.Len(t, result, 1)
-	assert.Equal(t, expected, result)
-
-	mockRepo.AssertExpectations(t)
+	repo.AssertExpectations(t)
 }
 
-func TestList_Empty(t *testing.T) {
-	mockRepo := new(mocks.Repository)
+func TestGetByID_RepositoryError(t *testing.T) {
+	repo := new(mocks.Repository)
 
-	svc := subscriptions.NewSubscriptionService(mockRepo)
-
-	mockRepo.
-		On("List", context.Background(), 10, 0).
-		Return([]model.Subscription{}, nil)
-
-	result, err := svc.List(
-		context.Background(),
-		10,
-		0,
-	)
-
-	assert.NoError(t, err)
-	assert.Empty(t, result)
-
-	mockRepo.AssertExpectations(t)
-}
-
-// Update tests
-func TestUpdate_Success(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-
-	svc := subscriptions.NewSubscriptionService(mockRepo)
+	svc := subscriptions.NewSubscriptionService(repo)
 
 	id := uuid.New()
 
-	price := 400
+	expectedErr := apperrors.ErrInternal
 
-	update := model.SubscriptionUpdate{
-		Price: &price,
-	}
-
-	mockRepo.
-		On("Update", context.Background(), id, update).
-		Return(nil)
-
-	err := svc.Update(
-		context.Background(),
+	repo.On("GetByID",
+		mock.Anything,
 		id,
-		update,
-	)
+	).Return(nil, expectedErr)
 
-	assert.NoError(t, err)
-
-	mockRepo.AssertExpectations(t)
-}
-
-func TestUpdate_RepositoryError(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-
-	svc := subscriptions.NewSubscriptionService(mockRepo)
-
-	id := uuid.New()
-
-	price := 400
-
-	update := model.SubscriptionUpdate{
-		Price: &price,
-	}
-
-	expectedErr := errors.New("db error")
-
-	mockRepo.
-		On("Update", context.Background(), id, update).
-		Return(expectedErr)
-
-	err := svc.Update(
-		context.Background(),
-		id,
-		update,
-	)
-
-	assert.Error(t, err)
-	assert.Equal(t, expectedErr, err)
-
-	mockRepo.AssertExpectations(t)
-}
-
-// Delete tests
-func TestDelete_Success(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-
-	svc := subscriptions.NewSubscriptionService(mockRepo)
-
-	id := uuid.New()
-
-	mockRepo.
-		On("Delete", context.Background(), id).
-		Return(nil)
-
-	err := svc.Delete(
-		context.Background(),
-		id,
-	)
-
-	assert.NoError(t, err)
-
-	mockRepo.AssertExpectations(t)
-}
-
-func TestDelete_RepositoryError(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-
-	svc := subscriptions.NewSubscriptionService(mockRepo)
-
-	id := uuid.New()
-
-	expectedErr := errors.New("delete failed")
-
-	mockRepo.
-		On("Delete", context.Background(), id).
-		Return(expectedErr)
-
-	err := svc.Delete(
+	sub, err := svc.GetByID(
 		context.Background(),
 		id,
 	)
 
 	assert.Error(t, err)
-	assert.Equal(t, expectedErr, err)
 
-	mockRepo.AssertExpectations(t)
-}
-
-// Total tests
-func TestTotal_Success(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-
-	svc := subscriptions.NewSubscriptionService(mockRepo)
-
-	from := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	to := time.Date(2025, time.December, 1, 0, 0, 0, 0, time.UTC)
-
-	mockRepo.
-		On("Total", context.Background(), (*uuid.UUID)(nil), (*string)(nil), from, to).
-		Return(2000, nil)
-
-	total, err := svc.Total(
-		context.Background(),
-		nil,
-		nil,
-		from,
-		to,
-	)
-
-	assert.NoError(t, err)
-
-	assert.Equal(t, 2000, total)
-
-	mockRepo.AssertExpectations(t)
-}
-
-func TestTotal_RepositoryError(t *testing.T) {
-	mockRepo := new(mocks.Repository)
-
-	svc := subscriptions.NewSubscriptionService(mockRepo)
-
-	from := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	to := time.Date(2025, time.December, 1, 0, 0, 0, 0, time.UTC)
-
-	expectedErr := errors.New("repository error")
-
-	mockRepo.
-		On("Total", context.Background(), (*uuid.UUID)(nil), (*string)(nil), from, to).
-		Return(0, expectedErr)
-
-	total, err := svc.Total(
-		context.Background(),
-		nil,
-		nil,
-		from,
-		to,
-	)
-
-	assert.Error(t, err)
-
-	assert.Equal(t, 0, total)
+	assert.Nil(t, sub)
 
 	assert.Equal(t, expectedErr, err)
 
-	mockRepo.AssertExpectations(t)
+	repo.AssertExpectations(t)
+}
+
+func TestGetByID_ContextCanceled(t *testing.T) {
+	repo := new(mocks.Repository)
+
+	svc := subscriptions.NewSubscriptionService(repo)
+
+	id := uuid.New()
+
+	ctx, cancel := context.WithCancel(
+		context.Background(),
+	)
+
+	cancel()
+
+	repo.On("GetByID",
+		mock.Anything,
+		id,
+	).Run(func(args mock.Arguments) {
+		ctx := args.Get(0).(context.Context)
+		<-ctx.Done()
+
+	}).Return(
+		nil,
+		apperrors.MapPostgresError(context.Canceled),
+	)
+
+	sub, err := svc.GetByID(
+		ctx,
+		id,
+	)
+
+	assert.Error(t, err)
+
+	assert.Nil(t, sub)
+
+	appErr, ok := err.(*apperrors.Error)
+
+	assert.True(t, ok)
+
+	assert.Equal(
+		t,
+		apperrors.ErrCanceled.Code,
+		appErr.Code,
+	)
+
+	repo.AssertExpectations(t)
+}
+
+func TestGetByID_ContextDeadlineExceeded(t *testing.T) {
+	repo := new(mocks.Repository)
+
+	svc := subscriptions.NewSubscriptionService(repo)
+
+	id := uuid.New()
+
+	repo.On("GetByID",
+		mock.Anything,
+		id,
+	).Run(func(args mock.Arguments) {
+		ctx := args.Get(0).(context.Context)
+		<-ctx.Done()
+	}).Return(
+		nil,
+		apperrors.MapPostgresError(context.DeadlineExceeded),
+	)
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		time.Millisecond,
+	)
+
+	defer cancel()
+
+	sub, err := svc.GetByID(
+		ctx,
+		id,
+	)
+
+	assert.Error(t, err)
+
+	assert.Nil(t, sub)
+
+	appErr, ok := err.(*apperrors.Error)
+
+	assert.True(t, ok)
+
+	assert.Equal(
+		t,
+		apperrors.ErrTimeout.Code,
+		appErr.Code,
+	)
+
+	repo.AssertExpectations(t)
 }
