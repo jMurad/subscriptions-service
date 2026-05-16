e1260d807c15d3ee42009abab755c71238e64db5
JMurad <jml.murad@gmail.com>
2026-05-17 01:10:45 +0300

    fix StatusNoContent

--- internal/transport/http/response/json.go
+++ internal/transport/http/response/json.go
@@ -5,18 +5,15 @@ import (
 	"net/http"
 )
 
-func JSON(
-	w http.ResponseWriter,
-	status int,
-	data any,
-) {
-
-	w.Header().Set(
-		"Content-Type",
-		"application/json",
-	)
-
+func JSON(w http.ResponseWriter, status int, data any) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
+	if status == http.StatusNoContent {
+		return
+	}
 
+	if data == nil {
+		return
+	}
 	_ = json.NewEncoder(w).Encode(data)
 }
