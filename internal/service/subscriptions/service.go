6ddb04e53525b6e7303b991c39ea8b7de1d38ffa
JMurad <jml.murad@gmail.com>
2026-05-16 19:43:40 +0300

    add GetByUserID to service

--- internal/service/subscriptions/service.go
+++ internal/service/subscriptions/service.go
@@ -127,6 +127,56 @@ func (s *SubService) GetByID(ctx context.Context, id uuid.UUID) (*model.Subscrip
 	return sub, nil
 }
 
+func (s *SubService) GetByUserID(ctx context.Context, userID uuid.UUID, limit, offset int) ([]model.Subscription, error) {
+	ctx, cancel := context.WithTimeout(ctx, mediumTimeout)
+	defer cancel()
+
+	log := logger.FromContext(ctx)
+
+	if limit <= 0 {
+		limit = 10
+	}
+
+	if limit > 100 {
+		limit = 100
+	}
+
+	if offset < 0 {
+
+		err := apperrors.WrapMessage(
+			nil,
+			apperrors.ErrValidation,
+			"offset must be greater than or equal to 0",
+		)
+
+		log.Warn("validation failed",
+			zap.String("field", "offset"),
+			zap.Error(err),
+		)
+
+		return nil, err
+	}
+
+	subscriptions, err := s.repo.GetByUserID(
+		ctx,
+		userID,
+		limit,
+		offset,
+	)
+	if err != nil {
+		log.Error("failed to get subscriptions by user id",
+			zap.String("user_id", userID.String()),
+			zap.Int("limit", limit),
+			zap.Int("offset", offset),
+			zap.Error(err),
+		)
+
+		return nil, err
+	}
+
+	return subscriptions, nil
+}
+
 func (s *SubService) List(ctx context.Context, limit, offset int) ([]model.Subscription, error) {
 	ctx, cancel := context.WithTimeout(ctx, mediumTimeout)
 	defer cancel()
