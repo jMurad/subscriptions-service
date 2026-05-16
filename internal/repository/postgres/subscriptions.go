6942b6eff677bb0661a7f9d6e9854ca0fc47ea00
JMurad <jml.murad@gmail.com>
2026-05-16 18:45:18 +0300

    add to repo pgGetByUserID method  with error handling

--- internal/repository/postgres/subscriptions.go
+++ internal/repository/postgres/subscriptions.go
@@ -103,6 +103,65 @@ func (r *SubRepo) GetByID(ctx context.Context, id uuid.UUID) (*model.Subscriptio
 	return &sub, nil
 }
 
+func (r *SubRepo) GetByUserID(ctx context.Context, userID uuid.UUID, limit, offset int) ([]model.Subscription, error) {
+	query := `
+    SELECT
+        id,
+        user_id,
+        service_name,
+        price,
+        start_date,
+        end_date,
+        created_at
+    FROM subscriptions
+    WHERE user_id = $1
+        AND deleted_at IS NULL
+    ORDER BY created_at DESC
+    LIMIT $2 OFFSET $3
+    `
+
+	rows, err := r.db.Query(
+		ctx,
+		query,
+		userID,
+		limit,
+		offset,
+	)
+	if err != nil {
+		return nil, apperrors.MapPostgresError(err)
+	}
+
+	defer rows.Close()
+
+	subscriptions := make([]model.Subscription, 0, limit)
+
+	for rows.Next() {
+
+		var sub model.Subscription
+
+		err = rows.Scan(
+			&sub.ID,
+			&sub.UserID,
+			&sub.ServiceName,
+			&sub.Price,
+			&sub.StartDate,
+			&sub.EndDate,
+			&sub.CreatedAt,
+		)
+		if err != nil {
+			return nil, apperrors.MapPostgresError(err)
+		}
+
+		subscriptions = append(subscriptions, sub)
+	}
+
+	if rows.Err() != nil {
+		return nil, apperrors.MapPostgresError(rows.Err())
+	}
+
+	return subscriptions, nil
+}
+
 func (r *SubRepo) List(ctx context.Context, limit, offset int) ([]model.Subscription, error) {
 		query := `
 	SELECT
