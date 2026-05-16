b6df9b231aae6110722bd99d42559b68f61279b4
JMurad <jml.murad@gmail.com>
2026-05-16 19:28:58 +0300

    refactor Create service add logic and error handling

--- internal/service/subscriptions/service.go
+++ internal/service/subscriptions/service.go
@@ -23,23 +23,74 @@ func NewSubscriptionService(repo repository.SubscriptionRepository) *SubService 
 }
 
 func (s *SubService) Create(ctx context.Context, sub model.Subscription) (uuid.UUID, error) {
+ctx, cancel := context.WithTimeout(ctx, shortTimeout)
+	defer cancel()
+
 	log := logger.FromContext(ctx)
-	start := time.Now()
+	
+	sub.ServiceName = strings.TrimSpace(sub.ServiceName)
 
-	log.Info("service create subscription started",
-		zap.String("service_name", sub.ServiceName),
-		zap.String("user_id", sub.UserID.String()),
-	)
+	if sub.ServiceName == "" {
+		err := apperrors.WrapMessage(
+			nil,
+			apperrors.ErrValidation,
+			"service_name is required",
+		)
 
-	id, err := s.repo.Create(ctx, sub)
-	if err != nil {
-		log.Error("service create subscription failed", zap.Error(err))
+		log.Warn("validation failed",
+		zap.String("field", "service_name"),
+			zap.Error(err),
+		)
+
 		return uuid.Nil, err
 	}
 
-	log.Info("service create subscription completed",
+	if sub.Price <= 0 {
+		err := apperrors.WrapMessage(
+			nil,
+			apperrors.ErrValidation,
+			"price must be greater than 0",
+		)
+
+		log.Warn("validation failed",
+		zap.String("field", "price"),
+			zap.Error(err),
+		)
+
+		return uuid.Nil, err
+	}
+
+	if sub.EndDate != nil &&
+		sub.EndDate.Before(sub.StartDate) {
+		err := apperrors.WrapMessage(
+			nil,
+			apperrors.ErrValidation,
+			"end_date must be greater than or equal to start_date",
+		)
+
+		log.Warn(
+			"validation failed",
+			zap.String("field", "end_date"),
+			zap.Error(err),
+	)
+
+		return uuid.Nil, err
+	}
+
+	id, err := s.repo.Create(ctx, sub)
+	if err != nil {
+		log.Error("failed to create subscription",
+			zap.String("user_id", sub.UserID.String()),
+			zap.String("service_name", sub.ServiceName),
+			zap.Error(err),
+		)
+
+		return uuid.Nil, err
+	}
+
+	log.Info("subscription created",
 		zap.String("subscription_id", id.String()),
-		zap.Duration("duration", time.Since(start)),
+		zap.String("user_id", sub.UserID.String()),
 	)
 
 	return id, nil
