04472c49b0a1154b806d4db71b7ce15bdc98bfb2
JMurad <jml.murad@gmail.com>
2026-05-18 11:55:26 +0300

    fix logger contextkey

--- internal/logger/context.go
+++ internal/logger/context.go
@@ -2,22 +2,21 @@ package logger
 
 import (
 	"context"
+	"subscriptions-service/internal/contextkeys"
 
 	"go.uber.org/zap"
 )
 
 type contextKey string
 
-const LoggerKey contextKey = "logger"
-
 // Put logger into context
 func ToContext(ctx context.Context, log *zap.Logger) context.Context {
-	return context.WithValue(ctx, LoggerKey, log)
+	return context.WithValue(ctx, contextkeys.Logger, log)
 }
 
 // Get logger from context
 func FromContext(ctx context.Context) *zap.Logger {
-	log, ok := ctx.Value(LoggerKey).(*zap.Logger)
+	log, ok := ctx.Value(contextkeys.Logger).(*zap.Logger)
 	if !ok || log == nil {
 		fallback, _ := zap.NewProduction()
 		return fallback
