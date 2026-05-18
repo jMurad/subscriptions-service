fc8ca43caba25ef65a059068d8d894be1a5f90e4
JMurad <jml.murad@gmail.com>
2026-05-18 12:16:55 +0300

    fix init app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,18 +1,39 @@
+// @title Subscriptions API
+// @version 1.0
+// @description REST API for managing subscriptions
+// @host localhost:8080
+// @BasePath /
 package main
 
 import (
-	"log"
-	_ "subscriptions-service/docs"
+	"context"
+
 	"subscriptions-service/internal/app"
+	"subscriptions-service/internal/config"
+	"subscriptions-service/internal/logger"
+
+	_ "subscriptions-service/docs"
+
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
-// @title Subscription Service API
-// @version 1.0
-// @description REST API для агрегации данных об онлайн подписках пользователей
-// @host localhost:8080
-// @BasePath /
 func main() {
-	if err := app.Run(); err != nil {
-		log.Fatal(err)
+	cfg := config.MustLoad()
+
+	log := logger.New()
+
+	db, err := pgxpool.New(context.Background(), cfg.DatabaseURL)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	providers := app.NewProviders(db)
+
+	router := app.NewRouter(log, providers.Handler)
+
+	server := app.NewServer(router)
+
+	if err := app.Run(server, log, db.Close); err != nil {
+		log.Fatal(err.Error())
 	}
 }
