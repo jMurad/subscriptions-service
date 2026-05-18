f1b9b9e2a219907f20b9b6bf82f29bb45e992a76
JMurad <jml.murad@gmail.com>
2026-05-18 12:14:17 +0300

    add timeouts

--- internal/app/server.go
+++ internal/app/server.go
@@ -2,27 +2,16 @@ package app
 
 import (
 	"net/http"
-	"subscriptions-service/internal/config"
-
-	"github.com/go-chi/chi/v5"
-	"go.uber.org/zap"
+	"time"
 )
 
-func newHTTPServer(cfg *config.Config, router *chi.Mux) *http.Server {
+func NewServer(handler http.Handler) *http.Server {
 	return &http.Server{
-		Addr:    ":" + cfg.AppPort,
-		Handler: router,
-	}
-}
-
-func runServer(logg *zap.Logger, srv *http.Server, port string) {
-	logg.Info("server started",
-		zap.String("port", port),
-	)
-
-	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logg.Fatal("server failed",
-			zap.Error(err),
-		)
+		Addr:              ":8080",
+		Handler:           handler,
+		ReadTimeout:       5 * time.Second,
+		WriteTimeout:      10 * time.Second,
+		IdleTimeout:       60 * time.Second,
+		ReadHeaderTimeout: 5 * time.Second,
 	}
 }
