c8e106239ebc05e0b9b314316b43d9fc615eaf24
JMurad <jml.murad@gmail.com>
2026-05-18 12:14:40 +0300

    fix router

--- internal/app/router.go
+++ internal/app/router.go
@@ -1,29 +1,30 @@
 package app
 
 import (
+	"net/http"
 	"subscriptions-service/internal/middleware"
+	"subscriptions-service/internal/transport/http/handler/subscriptions"
 
 	"github.com/go-chi/chi/v5"
 	httpSwagger "github.com/swaggo/http-swagger"
 	"go.uber.org/zap"
 )
 
-func newRouter(logg *zap.Logger, deps *Dependencies) *chi.Mux {
+func NewRouter(logg *zap.Logger, h *subscriptions.Handler) http.Handler {
 	r := chi.NewRouter()
 
-	r.Use(middleware.RequestID)
-	r.Use(middleware.Logger(logg))
-	r.Use(middleware.Recovery(logg))
+	middleware.Register(r, logg)
 
 	r.Get("/swagger/*", httpSwagger.WrapHandler)
 
 	r.Route("/subscriptions", func(r chi.Router) {
-		r.Post("/", deps.SubscriptionHandler.Create)
-		r.Get("/{id}", deps.SubscriptionHandler.GetByID)
-		r.Get("/", deps.SubscriptionHandler.List)
-		r.Get("/total", deps.SubscriptionHandler.Total)
-		r.Patch("/{id}", deps.SubscriptionHandler.Update)
-		r.Delete("/{id}", deps.SubscriptionHandler.Delete)
+		r.Post("/", h.Create)
+		r.Get("/", h.List)
+		r.Get("/user/{user_id}", h.GetByUserID)
+		r.Get("/{id}", h.GetByID)
+		r.Get("/total", h.Total)
+		r.Patch("/{id}", h.Update)
+		r.Delete("/{id}", h.Delete)
 	})
 
 	return r
