a309018ee9f01f73f06fc09284e816ea81b0414b
JMurad <jml.murad@gmail.com>
2026-05-18 12:08:04 +0300

    remove logging and fix struct

--- internal/app/providers.go
+++ internal/app/providers.go
@@ -1,76 +1,23 @@
 package app
 
 import (
-	"context"
-
-	"subscriptions-service/internal/config"
-	"subscriptions-service/internal/transport/http/handler"
-
-	// "subscriptions-service/internal/handler"
-
-	repo "subscriptions-service/internal/repository/postgres/subscriptions"
+	repository "subscriptions-service/internal/repository/postgres/subscriptions"
 	service "subscriptions-service/internal/service/subscriptions"
+	handler "subscriptions-service/internal/transport/http/handler/subscriptions"
 
 	"github.com/jackc/pgx/v5/pgxpool"
-	"go.uber.org/zap"
 )
 
-type Dependencies struct {
-	SubscriptionHandler *handler.SubscriptionHandler
+type Providers struct {
+	Handler *handler.Handler
 }
 
-func loadConfig(logg *zap.Logger) (*config.Config, error) {
-	logg.Info("loading config")
-
-	cfg, err := config.Load()
-	if err != nil {
-		logg.Error("failed to load config",
-			zap.Error(err),
-		)
-		return nil, err
-	}
-
-	logg.Info("config loaded")
-
-	return cfg, nil
-}
-
-func newLogger() (*zap.Logger, error) {
-	return zap.NewProduction()
-}
-
-func newPostgres(logg *zap.Logger, cfg *config.Config) (*pgxpool.Pool, error) {
-	logg.Info("connecting to postgres")
-
-	db, err := pgxpool.New(context.Background(), cfg.DBUrl)
-	if err != nil {
-		logg.Error("failed to connect postgres",
-			zap.Error(err),
-		)
-		return nil, err
-	}
-
-	logg.Info("postgres connected")
-
-	return db, nil
-}
-
-func initDependencies(logg *zap.Logger, db *pgxpool.Pool) *Dependencies {
-	logg.Info("initializing dependencies")
-
-	repo := repo.NewRepository(db)
-
-	logg.Info("repository initialized")
-
+func NewProviders(db *pgxpool.Pool) *Providers {
+	repo := repository.NewRepository(db)
 	svc := service.NewService(repo)
+	h := handler.NewHandler(svc)
 
-	logg.Info("service initialized")
-
-	h := handler.NewSubscriptionHandler(svc)
-
-	logg.Info("handler initialized")
-
-	return &Dependencies{
-		SubscriptionHandler: h,
+	return &Providers{
+		Handler: h,
 	}
 }
