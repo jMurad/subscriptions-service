498ea236aa71abe68d042cafe1353787773a3dcb
JMurad <jml.murad@gmail.com>
2026-05-18 12:15:34 +0300

    fix runner

--- internal/app/app.go
+++ internal/app/app.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"os"
 	"os/signal"
@@ -11,88 +12,47 @@ import (
 	"go.uber.org/zap"
 )
 
-func Run() error {
-	// --- logger ---
-	logg, err := newLogger()
-	if err != nil {
-		return err
-	}
+func Run(server *http.Server, log *zap.Logger, shutdownFns ...func()) error {
+	go func() {
+		log.Info("http server started",
+			zap.String("addr", server.Addr),
+		)
 
-	defer func() {
-		_ = logg.Sync()
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			log.Fatal("http server failed",
+				zap.Error(err),
+			)
+		}
 	}()
 
-	logg.Info("starting application")
-
-	// --- load environment ---
-	cfg, err := loadConfig(logg)
-	if err != nil {
-		return err
-	}
-
-	// --- DB ---
-	db, err := newPostgres(logg, cfg)
-	if err != nil {
-		return err
-	}
-
-	defer db.Close()
-
-	// --- repositories - services - handlers ---
-	deps := initDependencies(logg, db)
-
-	// --- router ---
-	router := newRouter(logg, deps)
-
-	// --- HTTP server ---
-	srv := newHTTPServer(cfg, router)
-
-	// --- start server ---
-	go runServer(logg, srv, cfg.AppPort)
-
-	// --- graceful shutdown ---
-	waitShutdownSignal(logg)
-
-	return shutdownServer(logg, srv)
-}
-
-func waitShutdownSignal(logg *zap.Logger) {
-	quit := make(chan os.Signal, 1)
+	stop := make(chan os.Signal, 1)
 
 	signal.Notify(
-		quit,
+		stop,
 		syscall.SIGINT,
 		syscall.SIGTERM,
 	)
 
-	logg.Info("waiting shutdown signal")
+	<-stop
 
-	sig := <-quit
+	log.Info("shutdown signal received")
 
-	logg.Info(
-		"shutdown signal received",
-		zap.String("signal", sig.String()),
-	)
-}
-
-func shutdownServer(logg *zap.Logger, srv *http.Server) error {
-	logg.Info("shutdown server")
-
-	ctx, cancel := context.WithTimeout(
-		context.Background(),
-		5*time.Second,
-	)
-
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	if err := srv.Shutdown(ctx); err != nil {
-		logg.Error("server shutdown failed",
+	if err := server.Shutdown(ctx); err != nil {
+		log.Error("http shutdown failed",
 			zap.Error(err),
 		)
+
 		return err
 	}
 
-	logg.Info("server stopped")
+	for _, fn := range shutdownFns {
+		fn()
+	}
+
+	log.Info("application stopped")
 
 	return nil
 }
